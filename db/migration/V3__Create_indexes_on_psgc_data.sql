128e13160d3c40d9c46ee8a6bee539283d902dc6
Gabriel Virrey <gabriel@ailuruslabs.dev>
2026-02-07 19:14:05 +0800

    Add PSGC Q4 2025 CSV data

---
+++ db/migration/V3__Create_indexes_on_psgc_data.sql
@@ -0,0 +1,7 @@
+create index idx_psgc_data_landing_city_class on psgc_data_q4_2025 (city_class);
+
+create index idx_psgc_data_landing_geographic_level on psgc_data_q4_2025 (geographic_level);
+
+create index idx_psgc_data_landing_urban_classification on psgc_data_q4_2025 (urban_classification);
+
+create index idx_psgc_data_landing_name on psgc_data_q4_2025 (name);
---
+++ db/migration/V4__Import_psgc_data.sql
@@ -0,0 +1,2 @@
+copy psgc_data_q4_2025 from '/var/lib/postgresql/data/psgc_data/PSGC-4Q-2025-Publication-Stripped.csv'
+    delimiter ',' csv header
