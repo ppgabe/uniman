abb7313f068e5a745522a6edad160aad7adc46c8
Gabriel Virrey <gabriel@ailuruslabs.dev>
2026-02-09 10:48:05 +0800

    Change method name to getByPersonId
    All persons in the database have an address. This commit adjusts the semantics so it implies this database constraint.

--- uniman-platform/uniman-domain/src/main/java/dev/ailuruslabs/uniman/domain/ports/output/AddressRepository.java
+++ uniman-platform/uniman-domain/src/main/java/dev/ailuruslabs/uniman/domain/ports/output/AddressRepository.java
@@ -1,8 +1,7 @@
 package dev.ailuruslabs.uniman.domain.ports.output;
 
-import dev.ailuruslabs.uniman.domain.common.Address;
+import dev.ailuruslabs.uniman.domain.common.locations.Address;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,7 +10,7 @@ public interface AddressRepository {
     void save(Address address);
 
     // TODO: Implement the Person class soon
-    Address findByPersonId(UUID personId);
+    Address getByPersonId(UUID personId);
     
     Optional<Address> findByUUID(UUID uuid);
 }
