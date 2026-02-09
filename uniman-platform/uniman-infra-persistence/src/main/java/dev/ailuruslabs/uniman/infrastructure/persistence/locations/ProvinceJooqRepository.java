230986420e9606cf91ea285199fa5d83a58d9b47
Gabriel Virrey <gabriel@ailuruslabs.dev>
2026-02-09 16:20:23 +0800

    Implement Province jOOQ repository methods

--- uniman-platform/uniman-infra-persistence/src/main/java/dev/ailuruslabs/uniman/infrastructure/persistence/locations/ProvinceJooqRepository.java
+++ uniman-platform/uniman-infra-persistence/src/main/java/dev/ailuruslabs/uniman/infrastructure/persistence/locations/ProvinceJooqRepository.java
@@ -9,11 +9,10 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static dev.ailuruslabs.uniman.infrastructure.persistence.jooq.Sequences.*;
+import static dev.ailuruslabs.uniman.infrastructure.persistence.jooq.Sequences.SEQ_PSGC_PROVINCES_ID;
+import static dev.ailuruslabs.uniman.infrastructure.persistence.jooq.Tables.PSGC_PROVINCES;
 
 public class ProvinceJooqRepository implements ProvinceRepository, IdentityGenerator<Integer> {
 
@@ -24,36 +23,57 @@ public class ProvinceJooqRepository implements ProvinceRepository, IdentityGener
     public ProvinceJooqRepository(DSLContext dsl) {
         this.dsl = dsl;
         this.sequenceCache = new ArrayDeque<>(20);
-        this.lock = new ReentrantLock(true);
+        this.lock = new ReentrantLock(); // No need for fair-ordering here. Not critical.
     }
 
     @Override
     public Integer nextIdentity() {
+
+        /* Acquire a lock first. I used a manual ReentrantLock over a synchronized block
+         * because prior to JDK 24, synchronized blocks get pinned to the carrier thread.
+         * This project uses JDK 25 as its target, but still.
+         */
         lock.lock();
         try {
+            // Check if the cache has values
             if (sequenceCache.isEmpty()) {
+                // If it is empty, fetch the next 20 IDs from the database
                 List<Integer> nextSequenceValues = dsl.nextvals(SEQ_PSGC_PROVINCES_ID, 20);
+                // Then add those to the cache
                 sequenceCache.addAll(nextSequenceValues);
             }
 
+            // Return the first ID in the queue
             return sequenceCache.poll();
         } finally {
+            // Release the lock
             lock.unlock();
         }
     }
 
     @Override
     public void save(Province province) {
-
+        dsl.insertInto(PSGC_PROVINCES)
+           .set(PSGC_PROVINCES.ID, province.getId())
+           .set(PSGC_PROVINCES.CODE, province.getCode())
+           .set(PSGC_PROVINCES.NAME, province.getName())
+           .set(PSGC_PROVINCES.REGION_CODE, province.getRegionCode())
+           .execute();
     }
 
     @Override
     public Optional<Province> findByCode(String code) {
-        return Optional.empty();
+        return dsl.selectFrom(PSGC_PROVINCES)
+                  .where(PSGC_PROVINCES.CODE.equal(code))
+                  .fetchOptional(record -> Province.reconstruct(record.getId(), record.getRegionCode(),
+                                                                record.getCode(), record.getName()));
     }
 
     @Override
     public List<Province> findAllByRegionCode(String regionCode) {
-        return List.of();
+        return dsl.selectFrom(PSGC_PROVINCES)
+                  .where(PSGC_PROVINCES.REGION_CODE.equal(regionCode))
+                  .fetch(record -> Province.reconstruct(record.getId(), record.getRegionCode(),
+                                                        record.getCode(), record.getName()));
     }
 }
